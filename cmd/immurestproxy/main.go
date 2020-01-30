75a2c2e1ef9fe4f267a749c638b06798e17357e7
Michele Meloni <cleaversdev@gmail.com>
2020-01-30 19:31:31 +0100

    feat: setup protobuffer rest proxy

---
+++ cmd/immurestproxy/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"github.com/rs/cors"
+	"google.golang.org/grpc"
+
+	gw "github.com/codenotary/immudbrestproxy/pkg/api"
+)
+
+var (
+	// command-line options:
+	// gRPC server endpoint
+	grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:8080", "gRPC server endpoint")
+)
+
+func run() error {
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// Register gRPC server endpoint
+	// Note: Make sure the gRPC server is running properly and accessible
+	mux := runtime.NewServeMux()
+
+	handler := cors.Default().Handler(mux)
+
+	opts := []grpc.DialOption{grpc.WithInsecure()}
+	err := gw.RegisterImmuServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
+	if err != nil {
+		return err
+	}
+
+	// Start HTTP server (and proxy calls to gRPC server endpoint)
+	return http.ListenAndServe(":8081", handler)
+}
+
+func main() {
+	flag.Parse()
+	defer glog.Flush()
+
+	if err := run(); err != nil {
+		glog.Fatal(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/codenotary/immudbrestproxy
+
+go 1.13
+
+require (
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b
+	github.com/golang/protobuf v1.3.3
+	github.com/grpc-ecosystem/grpc-gateway v1.12.2
+	github.com/rs/cors v1.7.0
+	google.golang.org/genproto v0.0.0-20200128133413-58ce757ed39b
+	google.golang.org/grpc v1.27.0
+)
